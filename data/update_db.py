ac53d6ece04d9874a186a1c6e1147702f1264df8
2awesome-rob <128756462+2awesome-rob@users.noreply.github.com>
2026-02-01 12:19:46 -0800

    update 2/1 database changes

--- data/update_db.py
+++ data/update_db.py
@@ -1,7 +1,5 @@
-import sqlite3
-
 #### stub for manually updating the database ####
-
+import sqlite3
 db_path = "data/HockeyStat.db"
 
 def describe_db_tables():
@@ -20,7 +18,25 @@ def describe_db_tables():
     conn.close()
     return
 
-#describe_db_tables()
+def update_player_active(game_id, player_id, active):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   UPDATE PlayerGameStats
+                   SET active=?
+                   WHERE game_id=? AND player_id=?
+                   """,(active,game_id,player_id))
+
+    if cursor.rowcount == 1:
+        print("✅ Update successful")
+    elif cursor.rowcount == 0:
+        print("🚫 No Record Found")
+    else:
+        print(f"Updated {cursor.rowcount} records, expected 1.")
+    
+    conn.commit()
+    conn.close()
 
 def add_team(team_id, club, season, team, location, coach):
     conn = sqlite3.connect(db_path)
@@ -35,12 +51,6 @@ def add_team(team_id, club, season, team, location, coach):
     conn.commit()
     conn.close()
 
-#add_team(10,'Tri-City Jr Americans','2025','Tolar Jr Americans','Hapo Center',None)
-#add_team(11,'Tri-City Jr Americans','2025','Geris Jr Americans','Hapo Center',None)
-#add_team(12,'Tri-City Jr Americans','2025','Knighten Jr Americans','Hapo Center',None)
-#add_team(13,'Sno-King Jr Thunderbirds','2025','Cooper Jr Thunderbirds','Sno-King Renton',None)
-#add_team(14,'Sno-King Jr Thunderbirds','2025','Trefethen Jr Thunderbirds','Sno-King Renton',None)
-
 def print_table_contents(table_name, game_id=None):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -52,14 +62,6 @@ def print_table_contents(table_name, game_id=None):
     for row in rows:
         print(row)
     conn.close()
-
-#print_table_contents("Games")
-#print_table_contents("Teams")
-#print_table_contents("Players")
-#print_table_contents("GoalieGameStats", 17)
-#print_table_contents("PlayerGameStats", 17)
-
-
 
 def add_game(game_id, date, home_team_id, home_score, away_team_id, away_score, league_play, game_type):
     conn = sqlite3.connect(db_path)
@@ -87,14 +89,6 @@ def add_game(game_id, date, home_team_id, home_score, away_team_id, away_score, 
     conn.commit()
     conn.close()
 
-#add_game(14, '2026-01-23', 10, 4, 1, 5, 0, 2)
-#add_game(15, '2026-01-23', 11, 6, 1, 2, 0, 2)
-#add_game(16, '2026-01-24', 1, 6, 12, 3, 0, 2)
-#add_game(17, '2026-01-24', 13, 1, 1, 2, 0, 2)
-#add_game(18, '2026-01-25', 14, 7, 1, 3, 0, 2)
-#add_game(19, '2026-01-31', 1, 8, 8, 7, 0, 1)
-
-
 def update_goalie_stats(game_id, player_id, shots_faced, saves, goals_allowed, active, result):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -114,7 +108,6 @@ def update_goalie_stats(game_id, player_id, shots_faced, saves, goals_allowed, a
     
     conn.commit()
     conn.close()
-
 
 def update_player_stats(game_id, player_id, goals, assists, penalty_min, active):
     conn = sqlite3.connect(db_path)
@@ -137,29 +130,38 @@ def update_player_stats(game_id, player_id, goals, assists, penalty_min, active)
     conn.close()
 
 
+#describe_db_tables()
+#add_team(14,'Sno-King Jr Thunderbirds','2025','Trefethen Jr Thunderbirds','Sno-King Renton',None)
+
+#print_table_contents("Games")
+#print_table_contents("Teams")
+#print_table_contents("Players")
+#print_table_contents("GoalieGameStats", 17)
+#print_table_contents("PlayerGameStats", 17)
+
+#add_game(19, '2026-01-31', 1, 8, 8, 7, 0, 1)
+#add_game(20, '2026-02-01', 5, 0, 1, 13, 0, 1)
+
 
 #update_goalie_stats(19, 4, 29, 22, 7, 1, "W")
 #update_player_stats(19, 4, 0, 0, 0, 0)
 #update_goalie_stats(19, 9, 0, 0, 0, 0, None)
-#update_goalie_stats(18, 9, 38, 31, 7, 1, "L")
-#update_player_stats(18, 9, 0, 0, 0, 0)
-#update_goalie_stats(18, 4, 0, 0, 0, 0, None)
+#update_goalie_stats(20, 9, 17, 17, 0, 1, "W")
+#update_player_active(20, 9, False)
+#update_goalie_stats(20, 4, 0, 0, 0, 0, None)
 
+#update_player_stats(20, 5, 4, 2, 0, 1)
+#update_player_stats(20, 1, 1, 2, 0, 1)
+#update_player_stats(20, 13, 2, 0, 0, 1)
+#update_player_stats(20, 11, 0, 1, 0, 1)
+#update_player_stats(20, 7, 3, 0, 0, 1)
+update_player_stats(20, 8, 0, 2, 0, 0)
+#update_player_stats(20, 2, 1, 1, 0, 0)
+#update_player_stats(20, 10, 1, 0, 0, 0)
+#update_player_stats(20, 4, 1, 0, 0, 0)
 
-#update_player_stats(18, 5, 3, 0, 0, 1)
-
-#update_player_stats(19, 5, 2, 1, 0, 1)
-#update_player_stats(19, 9, 3, 1, 0, 1)
-#update_player_stats(19, 2, 2, 0, 0, 1)
-#update_player_stats(19, 13, 1, 1, 0, 1)
-#update_player_stats(19, 3, 0, 0, 0, 0)
-
-#update_player_stats(17, 11, 0, 0, 1, 1)
-
-
-
-
-
+#update_player_active(20, 3, False)
+#update_player_active(20, 12, False)
 
 
 
@@ -168,34 +170,6 @@ def update_player_stats(game_id, player_id, goals, assists, penalty_min, active)
 
 
 
-
-
-
-
-
-
-def update_player_active(game_id, player_id, active):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    cursor.execute("""
-                   UPDATE PlayerGameStats
-                   SET active=?
-                   WHERE game_id=? AND player_id=?
-                   """,(active,game_id,player_id))
-
-    if cursor.rowcount == 1:
-        print("✅ Update successful")
-    elif cursor.rowcount == 0:
-        print("🚫 No Record Found")
-    else:
-        print(f"Updated {cursor.rowcount} records, expected 1.")
-    
-    conn.commit()
-    conn.close()
-
-#update_player_active(9, 9, False)
-#update_player_active(7, 3, True)
 
 
 def add_column(tbl, col):
@@ -207,7 +181,6 @@ def add_column(tbl, col):
     print(f"Added column {col} to table {tbl}")
     conn.commit()
     conn.close()
-
 #add_column("Games", "game_type")
 
 def drop_column(tbl, col):
@@ -218,7 +191,6 @@ def drop_column(tbl, col):
     
     conn.commit()
     conn.close()
-
 #drop_column("Games", "league_play")
 
 def update_game_type(game_id, game_type):
@@ -240,5 +212,4 @@ def update_game_type(game_id, game_type):
     
     conn.commit()
     conn.close()
-
 #update_game_type(14,2)
